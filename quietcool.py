43a662c6b88870cb3b0aaf4e00e8bf82a1c0fc7c
Sam Quigley <quigley@emerose.com>
2024-12-09 09:59:57 -0500

    gussied up and ready for publication

--- quietcool.py
+++ quietcool.py
@@ -28,12 +28,29 @@ async def main(command: str, api_id: Optional[str] = None) -> None:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Quietcool Client')
+    parser = argparse.ArgumentParser(
+        description='Quietcool Client\n\n'
+                    'Connects to a QuietCool Wireless RF Control Kit via BLE\n\n'
+                    'Commands:\n'
+                    '  info: Dumps detailed information about the connected fan\n'
+                    '  pair: Pairs the client with a fan (fan must be in pairing mode)\n\n'
+                    'API ID:\n'
+                    '  An API ID is required to connect to the fan. \n'
+                    '  If no --id is provided, the API ID will be sourced in this order:\n'
+                    '    1. QUIETCOOL environment variable\n'
+                    '    2. /etc/quietcool file\n'
+                    '    3. ~/.quietcool file\n'
+                    '    4. ./.quietcool file\n\n'
+                    'PAIRING:\n'
+                    '  The API ID must be paired with the fan. In order to pair the API ID\n'
+                    '  with the fan, set the fan to pairing mode on another device or push the\n'
+                    '  "Pair" button on the controller. Then run the client with the pair command.\n\n',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('command', nargs='?', default='info',
-                        help='Command to execute:\n'
-                             '  info: Dumps detailed information about the connected fan (default)\n'
-                             '  pair: Pairs the client with a fan (fan must be in pairing mode)')
-    parser.add_argument('--id', help='API ID string', default=None)
+                        help='Command to execute (info or pair)')
+    parser.add_argument('--id',
+                        help='API ID string (see description for details)',
+                        default=None)
     parser.add_argument(
         '--log-level',
         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
